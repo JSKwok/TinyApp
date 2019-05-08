70b5e21e3afb652a0f7e15707a4b81ad04bbf482
Justin <justin.s.kwok@gmail.com>
2019-05-08 00:03:26 +0000

    Added submission form, installed body parse, created GET route to retrieve form, create POST route to get input, added random number generator function

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ var app = express();
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,20 +19,45 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//Get route going to URL database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//Get route going to URLs page
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", { urls: urlDatabase });
 });
 
+//Get route going to new URL page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//Get route going to page for a praticular URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+function generateRandomString() {
+  let output = "";
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < 6; i++) {
+    output += alphanumeric.charAt(Math.floor(Math.random() * alphanumeric.length));
+  };
+  return output;
+};
+
+console.log(generateRandomString());
